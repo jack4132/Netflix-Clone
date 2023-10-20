0670b81cd521a85c01f088762e56367dade4308a
Jackson <jackson.thounaojam@mutualmobile.com>
2023-10-20 12:42:17 +1100

    logo fixed

--- src/Nav.js
+++ src/Nav.js
@@ -1,29 +1,23 @@
-import React, { useState,useEffect } from 'react';
+import React, {useState, useEffect} from "react";
 import "./Nav.css";
 
 function Nav() {
-    const [show,handleShow] = useState(false);
-    useEffect(()=>{
-        window.addEventListener("scroll", ()=>{
-            if(window.scrollY > 50){
-                handleShow(true);
-                console.log(show)
-            }else handleShow(false);
-        });
-        
-    },[]); /* this is the effect of header netflix logo & avatar which will show its background after 100px i.e. mentioned in window.scrollY. */
-    return (
-        <div className={`nav ${show && "nav__black"}`}>
-            <img className="nav__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Netflix_2014_logo.svg"
-            alt="Netflix Logo"/>
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        handleShow(true);
+        console.log(show);
+      } else handleShow(false);
+    });
+  }, []); /* this is the effect of header netflix logo & avatar which will show its background after 100px i.e. mentioned in window.scrollY. */
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src="/logo.avif" alt="Netflix Logo" />
 
-            <img className="nav__avatar"
-            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-            alt="Netflix Logo"/>
-            
-        </div>
-    )
+      <img className="nav__avatar" src="/user.jpeg" alt="Netflix Logo" />
+    </div>
+  );
 }
 
 export default Nav;
